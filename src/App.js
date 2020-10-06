a8d5726766c0588ec57cff86ac8b9f01e6544565
edgarwu102999508 <102999508@student.swin.edu.au>
2020-10-06 22:40:59 +1100

    Refactoring to Hooks & Context

--- src/App.js
+++ src/App.js
@@ -1,107 +1,35 @@
-import React, { useState, Fragment } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-import axios from "axios";
 import Navbar from "./components/layout/Navbar";
-import Users from "./components/users/Users";
 import User from "./components/users/User";
+import Home from "./components/pages/Home";
 import About from "./components/pages/About";
-import Search from "./components/users/Search";
+import NotFound from "./components/pages/NotFound";
+
 import Alert from "./components/layout/Alert";
 
+import GithubState from "./context/github/GithubState";
+import AlertState from "./context/alert/AlertState";
+
 const App = () => {
-  const [users, setUsers] = useState([]);
-  const [user, setUser] = useState({});
-  const [repos, setRepos] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [alert, setAlert] = useState(null);
-
-  //Search Github Users
-  const searchUsers = async text => {
-    setLoading(true);
-    const response = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-    setUsers(response.data.items);
-    setLoading(false);
-  };
-
-  // Get single Github User
-  const getUser = async username => {
-    setLoading(true);
-    const response = await axios.get(
-      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-    setUser(response.data);
-    setLoading(false);
-  };
-
-  // Get single Github User Repos
-  const getUserRepos = async username => {
-    setLoading(true);
-    const response = await axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-    setRepos(response.data);
-    setLoading(false);
-  };
-
-  // Clear Users from state
-  const clearUsers = () => {
-    setUsers([]);
-    setLoading(false);
-  };
-
-  // Set Alert Popup
-  const showAlert = (msg, type) => {
-    setAlert({ msg, type });
-    setTimeout(() => {
-      setAlert(null);
-    }, 5000);
-  };
-
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <div className='container'>
-          <Alert alert={alert} />
-          <Switch>
-            <Route
-              exact
-              path='/'
-              render={props => (
-                <Fragment>
-                  <Search
-                    {...props}
-                    searchUsers={searchUsers}
-                    clearUsers={clearUsers}
-                    showClearBtn={users.length > 0 ? true : false}
-                    setAlert={showAlert}
-                  />
-                  <Users loading={loading} users={users} />
-                </Fragment>
-              )}
-            />
-            <Route exact path='/about' component={About} />
-            <Route
-              exact
-              path='/user/:username'
-              render={props => (
-                <User
-                  {...props}
-                  getUser={getUser}
-                  getUserRepos={getUserRepos}
-                  user={user}
-                  repos={repos}
-                  loading={loading}
-                />
-              )}
-            />
-          </Switch>
-        </div>
-      </Fragment>
-    </Router>
+    <GithubState>
+      <AlertState>
+        <Router>
+          <Navbar />
+          <div className='container'>
+            <Alert />
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/about' component={About} />
+              <Route exact path='/user/:username' component={User} />
+              <Route component={NotFound} />
+            </Switch>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
   );
 };
 
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,10 +1,13 @@
-import React, { useEffect, Fragment } from "react";
+import React, { useEffect, Fragment, useContext } from "react";
 import Spinner from "../layout/Spinner";
 import Repos from "../repos/Repos";
-import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import GithubContext from "../../context/github/githubContext";
 
-const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
+const User = ({ match }) => {
+  const githubContext = useContext(GithubContext);
+  const { user, loading, getUser, repos, getUserRepos } = githubContext;
+
   useEffect(() => {
     getUser(match.params.username);
     getUserRepos(match.params.username);
@@ -107,13 +110,6 @@ const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
       <Repos repos={repos} />
     </Fragment>
   );
-};
-User.propTypes = {
-  loading: PropTypes.bool.isRequired,
-  user: PropTypes.object.isRequired,
-  repos: PropTypes.array.isRequired,
-  getUser: PropTypes.func.isRequired,
-  getUserRepos: PropTypes.func.isRequired,
 };
 
 export default User;
